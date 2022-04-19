c622adda10559eb3c693943185c00be1a93e2a9c
M-Julius <mjulius.dev@gmail.com>
2022-04-19 21:45:28 +0700

    add documentation in code

--- Application.vb
+++ Application.vb
@@ -9,6 +9,7 @@ Imports System.Collections
 ' and then showing total price
 
 Module ModuleP4
+    ' make structure of product
     Structure Product
         Dim name As String
         Dim price As Integer
@@ -16,6 +17,7 @@ Module ModuleP4
 
 
     Sub Main()
+        ' define main variable
         Dim Products As New ArrayList
         Dim p As Product
         Dim lineSpace As String = "========================================"
@@ -25,32 +27,43 @@ Module ModuleP4
 
         Dim i As Integer = &B10
 
+        ' loop for input product
         Do While (i > 1)
+            ' input product
             p = addProduct()
+
+            ' add product to arraylist
             Products.Add(p)
 
+            ' ask user to add new product
             Console.WriteLine("Tambah Belanja (YES/NO) : ")
             answer = Console.ReadLine()
 
+            ' when user answer 'YES' repeat this action
             If answer = "YES" Then
                 Console.WriteLine(lineSpace)
                 Console.WriteLine("Tambah Belanjaan Baru")
                 i += 1
             Else
+                ' when user answer 'NO' stop this action
                 i = 1
             End If
         Loop
 
         Console.WriteLine(lineSpace)
         Console.WriteLine("Semua Belanjaan : ")
+        
+        ' loop for showing all product and calculate total price
         For Each p In Products
             Console.WriteLine(p.name & " - " & p.price)
             totalPrice += p.price
         Next p
 
+        ' show total price
         Console.WriteLine("Total Belanja : " & totalPrice)
     End Sub
 
+    ' function for input product
     Function addProduct() As Product
         Dim p As Product
 
@@ -60,6 +73,7 @@ Module ModuleP4
         Console.WriteLine("Harga Barang : ")
         p.price = Integer.Parse(Console.ReadLine())
 
+        ' return product
         Return p
     End Function
 End Module
